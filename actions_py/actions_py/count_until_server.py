105a3ce838b564a71965ccdd070eee3332dc3ced
AshishRamesh <ashishramesh2003@gmail.com>
2024-05-31 11:35:53 +0530

    First action server created

--- actions_py/actions_py/count_until_server.py
+++ actions_py/actions_py/count_until_server.py
@@ -1,11 +1,38 @@
 #!/usr/bin/env python3
 import rclpy
+import time
 from rclpy.node import Node
+from rclpy.action import ActionServer
+from rclpy.action.server import ServerGoalHandle
 from my_robot_interfaces.action import CountUntil
 
 class CountUntilServerNode(Node): # MODIFY NAME
     def __init__(self):
         super().__init__("count_until_server") # MODIFY NAME
+        self.count_until_server_ = ActionServer(self,
+            CountUntil,
+            "count_until",
+            execute_callback=self.execute_callback)
+        self.get_logger().info("Action Server has started......")
+        
+    def execute_callback(self, goal_handle: ServerGoalHandle):
+        target_number = goal_handle.request.target_number
+        period = goal_handle.request.period
+
+        self.get_logger().info("Executing the Goal!!!!!!!!")
+        counter = 0
+
+        for i in range(target_number):
+            counter +=1
+            self.get_logger().info("Counter is : "+ str(counter))
+            time.sleep(period)
+        
+        goal_handle.succeed()
+
+        result = CountUntil.Result()
+        result.reached_number = counter
+        return result
+
 
 
 def main(args=None):
