028cb00d459d7d66833e5d23e9ed1e6f28fbf0b9
AshishRamesh <ashishramesh2003@gmail.com>
2024-06-07 15:24:03 +0530

    cpp action sever

---
+++ actions_cpp/src/count_until_server.cpp
@@ -0,0 +1,21 @@
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+
+class CountUntilServerNode : public rclcpp::Node 
+{
+public:
+    CountUntilServerNode() : Node("count_until_server") 
+    {
+    }
+
+private:
+};
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<CountUntilServerNode>(); 
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
